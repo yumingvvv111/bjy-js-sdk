72a9aa828776d9a6972a797e464ed70c39f7bb72
musicode <musicode@qq.com>
2017-11-02 14:53:06 +0800

    udpate

--- h5/index.js
+++ h5/index.js
@@ -181,25 +181,6 @@ $(document).ready(function () {
                         rowPerPage: 4,
                     }
                 });
-                barrage = new BJY.Barrage({
-                    container: $('#barrage'),
-                    // 移动速度，每秒移动多少个像素
-                    speed: 100,
-                    // 最长可显示多少个字，超过这个字数的会过滤
-                    maxLength: 20,
-                    // 在 top - bottom 的区间出现弹幕
-                    top: 30,
-                    bottom: 30,
-                    // 轨道高度
-                    trackHeight: 30,
-                    // 是否只显示文本消息
-                    textOnly: false,
-                    // 如果需要自定义弹幕内容，可实现此方法
-                    renderContent: function (data) {
-                        return data.content;
-                    }
-                });
-                barrage.open();
 
                 mediaData.setSpeakerVolume(100);
 
@@ -213,19 +194,19 @@ $(document).ready(function () {
                     $('.video').css('display', 'block');
                 });
 
-                if (isIOS) {
-                    playButton.removeClass('hidden');
-                    playButton.on('click', function () {
-                        // 如果当前播放器正在播放，先关掉播放器
-                        if (teacherH5Player.videoOn || teacherH5Player.audioOn) {
-                            teacherH5Player.playAVClose();
-                        }
-                        // 播放视频
-                        teacherH5Player.playAV(
-                            store.get('teacher.videoOn')
-                        );
-                    });
-                }
+                // if (isIOS) {
+                //     playButton.removeClass('hidden');
+                //     playButton.on('click', function () {
+                //         // 如果当前播放器正在播放，先关掉播放器
+                //         if (teacherH5Player.videoOn || teacherH5Player.audioOn) {
+                //             teacherH5Player.playAVClose();
+                //         }
+                //         // 播放视频
+                //         teacherH5Player.playAV(
+                //             store.get('teacher.videoOn')
+                //         );
+                //     });
+                // }
             }
         )
         .on(
